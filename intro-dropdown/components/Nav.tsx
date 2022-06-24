1e5e7c31970984183608a3c4b42f444fe852829f
Kyle <Kyle.fire@gmail.com>
2022-06-24 13:41:42 +0900

    fixed minor issue with breakpoints

--- intro-dropdown/components/Nav.tsx
+++ intro-dropdown/components/Nav.tsx
@@ -20,14 +20,14 @@ export default function Nav(props): JSX.Element {
   }
 
   return (
-    <nav className='flex gap-4 px-4 py-6 bg-green-400'>
-      <div className='flex items-center justify-between w-full'>
+    <nav className='flex gap-4 px-4 py-6 bg-blue-300'>
+      <div className='flex items-center justify-between w-full bg-green-300'>
         <div className='flex items-center gap-10'>
           <div>
             <Logo />
           </div>
-          <div className='flex invisible gap-10 lg:visible'>
-            <div className='flex items-center gap-2'>
+          <div className='hidden gap-10 lg:flex'>
+            <div className='flex items-center gap-2 lg:hidden'>
               <button
                 onClick={menuHelper}
                 name='features'
