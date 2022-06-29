91cac744a861c3b9d991fdc8b6a5ddafaa544782
Kyle <Kyle.fire@gmail.com>
2022-06-29 10:15:32 +0900

    layout done

--- intro-dropdown/components/Body.tsx
+++ intro-dropdown/components/Body.tsx
@@ -3,11 +3,12 @@ import DataBiz from '../public/photos/client-databiz.svg';
 import Audiophile from '../public/photos/client-audiophile.svg';
 import Meet from '../public/photos/client-meet.svg';
 import Maker from '../public/photos/client-maker.svg';
+import desktopHero from '../public/photos/image-hero-desktop.png';
 
 export default function Body() {
   return (
     <div className='h-full'>
-      <div className='flex flex-col items-center w-full h-full py-8 border-2 border-pink-300 lg:py-20 lg:flex-row '>
+      <div className='flex flex-col items-center w-full h-full py-8 lg:flex-row '>
         <div className='lg:hidden'>
           <Image
             width={750}
@@ -18,8 +19,8 @@ export default function Body() {
             alt='mobile hero'
           />
         </div>
-        <div className='flex flex-col items-center justify-between w-full h-full border-2 border-green-400 lg:justify-between lg:items-start lg:m-20 lg:p-8 lg:w-1/2'>
-          <p className='text-4xl font-bold lg:my-6 lg:pr-8 lg:text-8xl'>
+        <div className='flex flex-col items-center justify-between w-full h-full lg:justify-between lg:items-start lg:m-20 lg:p-8 lg:w-1/2'>
+          <p className='pt-8 text-4xl font-bold lg:my-6 lg:pr-8 lg:text-8xl'>
             Make remote work
           </p>
           <p className='px-4 py-8 text-center text-gray-500 lg:text-xl lg:max-w-lg lg:px-0 lg:py-0 lg:text-left'>
@@ -37,12 +38,13 @@ export default function Body() {
           </div>
         </div>
 
-        <div className='hidden lg:flex'>
+        <div className='hidden lg:max-h-fit lg:flex'>
           <Image
-            width={960}
-            height={1280}
+            width={960 / 2}
+            height={1280 / 2}
             layout='intrinsic'
-            src='/photos/image-hero-desktop.png'
+            objectFit='scale-down'
+            src={desktopHero}
             alt='desktop hero'
           />
         </div>
